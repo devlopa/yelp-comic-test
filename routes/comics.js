18959c9ac5e43e099aa7c124de5bceb9a4cb3ba8
Shehryar Ahmad <sakhan24@gmail.com>
2021-08-11 18:47:49 +0500

    Updating Navbar and adding genre route

--- routes/comics.js
+++ routes/comics.js
@@ -34,6 +34,17 @@ router.get('/search',async(req,res)=>{
     }
 })
 
+
+router.get('/genre/:genre',async(req,res)=>{
+    const validGenres = ["superhero","manga","slice-of-life","humor","sci-fi","fantasy","horror","action","nonfiction"];
+    if(validGenres.includes(req.params.genre.toLowerCase())){
+        const comics = await Comic.find({genre:req.params.genre}).exec();
+        res.render('comics',{comics});
+    }else{
+        res.send("Please enter a Valid Genre")
+    }
+});
+
 router.get('/:id',async (req,res)=>{
    try{
 
